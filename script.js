48214bbb8131bb2e1a871aade348a35d6171c185
Silva-Gabriel <pottergryff8@gmail.com>
2025-04-17 00:05:38 -0300

    feat: ajustes na tela de login, criação da tela de cadastro e início das validações dos campos

---
+++ script.js
@@ -0,0 +1,163 @@
+document.getElementById('cpf').addEventListener('input', validateCPF);
+// TODO: Criar as validações restantes
+//  document.getElementById('name').addEventListener('input', validateName)...
+
+function validateCPF(event) {
+    const cpf = event.target.value;
+    const element = event.target;
+    const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+
+    // Se o campo estiver vazio, remova as classes de validação
+    if (!cpf) {
+        clearValidationStyles(element);
+        return;
+    }
+
+    // Aplica estilos com base na validação
+    if (cpfRegex.test(cpf)) {
+        resetElementStyle(element); // Campo válido
+    } else {
+        applyErrorStyle(element); // Campo inválido
+    }
+}
+
+function resetElementStyle(element) {
+    element.classList.remove('invalid');
+    element.classList.add('valid');
+}
+
+function applyErrorStyle(element) {
+    element.classList.remove('valid');
+    element.classList.add('invalid');
+}
+
+function clearValidationStyles(element) {
+    element.classList.remove('valid', 'invalid');
+}
+
+async function LogIn(event) {
+  event.preventDefault();
+
+  // elements
+  let user = document.getElementById("user");
+  let password = document.getElementById("pwd");
+
+  // values
+  let userValue = user.value;
+  let passwordValue = password.value;
+
+  fetch("../data.json")
+    .then((response) => response.json())
+    .then(async (data) => {
+      let users = data.users;
+      let usernames = Object.keys(users);
+      let matchedUser = usernames.find((username) => {
+        return (
+          username === userValue && users[username].password === passwordValue
+        );
+      });
+
+      if (!matchedUser) {
+        let popup = CreateElement('div', 'popup-error', 'Usuário ou senha incorretos!');
+        RemovePopUpOfConsole(popup);
+      } else {
+        let popup = CreateElement('div', 'popup-sucess', 'Login realizado com sucesso!');
+        RemovePopUpOfConsole(popup);
+        await Sleep(1800);
+        window.location.href = "home-club.html";
+      }
+    })
+    .catch((error) => console.error("Erro ao carregar os usuários:", error));
+}
+
+async function SignUp(event) {
+  event.preventDefault();
+  console.log("entrou no signup");
+
+  // elements
+  let name = document.getElementById("name");
+  let cpf = document.getElementById("cpf");
+  let email = document.getElementById("email");
+  let user = document.getElementById("user");
+  let password = document.getElementById("pwd");
+  let passwordConfirm = document.getElementById("confirm-pwd");
+
+  console.log(name);
+
+  // values
+  let nameValue = name.value;
+  let cpfValue = cpf.value;
+  let emailValue = email.value;
+  let userValue = user.value;
+  let passwordValue = password.value;
+  let passwordConfirmValue = passwordConfirm.value;
+
+  // validations
+  let passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+  let userRegex = /^[a-zA-Z0-9]+$/;
+  let requiredFields = [
+    nameValue,
+    cpfValue,
+    emailValue,
+    userValue,
+    passwordValue,
+    passwordConfirmValue,
+  ];
+  let emptyFields = requiredFields.filter((field) => field == '');
+  console.log(emptyFields);
+
+  if (emptyFields.length > 0) {
+    let popup = CreateElement('div', 'popup-error', 'Existem campos vazios!<br>Preencha todos os campos.');
+    RemovePopUpOfConsole(popup);
+    return;
+  }
+  if (!cpfRegex.test(cpfValue)) {
+    let popup = CreateElement('div', 'popup-error', 'CPF inválido!<br>O CPF deve estar no formato xxx.xxx.xxx-xx.');
+    RemovePopUpOfConsole(popup);
+    return;
+  } 
+  else if (!passwordRegex.test(passwordValue)) {
+    let popup = CreateElement('div', 'popup-error', 'Sua senha é fraca! Ela deve conter mais de 8 caracteres, ao menos um especial, uma letra e um número.');
+    RemovePopUpOfConsole(popup);
+    return;
+  } 
+  else if (passwordValue === passwordConfirmValue) {
+    let popup = CreateElement('div', 'popup-error', 'Cadastro realizado com sucesso!<br>Você será redirecionado para a página de login.');
+    RemovePopUpOfConsole(popup);
+
+    // Outras validações
+    // Salvar os dados em memória
+    await Sleep(1800);
+    window.location.href = "index.html";
+  } 
+  else {
+    let popup = CreateElement('div', 'popup-error', 'As senhas não coincidem!');
+    RemovePopUpOfConsole(popup);
+  }
+}
+
+function Sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function CreateElement(element, className, content) {
+  {
+    let x = document.createElement(element);
+    x.className = className;
+    x.innerHTML = content;
+    document.body.appendChild(x);
+
+    return x;
+  }
+}
+
+function RemovePopUpOfConsole(popup)
+{
+    popup.addEventListener("animationend", (event) => {
+        if (event.animationName === "fadeOut") {
+            popup.remove();
+        }
+    });
+}
