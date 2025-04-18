44a61fe3cd2928c5dc28339cce50f5e02287a201
Gabriel S S <pottergryff8@gmail.com>
2025-04-18 04:45:21 -0300

    feat: finalização das validações nos campos do cadastro

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-document.getElementById('cpf').addEventListener('input', validateCPF);
-// TODO: Criar as validações restantes
-//  document.getElementById('name').addEventListener('input', validateName)...
+function validateName(event) {
+    const name = event.target.value;
+    const element = event.target;
+    const nameRegex = /^[a-zA-ZÀ-ÿ\s]+$/; // Validar nomes com letras e espaços
+    const hasMultipleWords = name.trim().split(/\s+/).length > 1;
+
+    validateElement(name, nameRegex.test(name) && hasMultipleWords, element);
+}
 
 function validateCPF(event) {
     const cpf = event.target.value;
     const element = event.target;
     const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
 
-    // Se o campo estiver vazio, remova as classes de validação
-    if (!cpf) {
+    validateElement(cpf, cpfRegex.test(cpf), element);
+}
+
+function validateEmail(event) {
+    const email = event.target.value;
+    const element = event.target;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    validateElement(email, emailRegex.test(email), element);
+}
+
+function validateUser(event) {
+    const user = event.target.value;
+    const element = event.target;
+    const userRegex = /^[a-zA-Z][a-zA-Z0-9]*$/;
+
+    validateElement(user, userRegex.test(user), element);
+}
+
+function validateElement(value, expression, element) {
+    if (!value) {
         clearValidationStyles(element);
         return;
     }
 
-    // Aplica estilos com base na validação
-    if (cpfRegex.test(cpf)) {
-        resetElementStyle(element); // Campo válido
+    if (expression) {
+        resetElementStyle(element);
     } else {
-        applyErrorStyle(element); // Campo inválido
+        applyErrorStyle(element);
     }
 }
 
@@ -34,6 +57,11 @@ function applyErrorStyle(element) {
 function clearValidationStyles(element) {
     element.classList.remove('valid', 'invalid');
 }
+
+document.getElementById('cpf').addEventListener('input', validateCPF);
+document.getElementById('name').addEventListener('input', validateName);
+document.getElementById('email').addEventListener('input', validateEmail);
+document.getElementById('user').addEventListener('input', validateUser);
 
 async function LogIn(event) {
   event.preventDefault();
@@ -122,11 +150,13 @@ async function SignUp(event) {
   let passwordConfirmValue = passwordConfirm.value;
 
   // validations
-  let passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
-  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  let cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-  let userRegex = /^[a-zA-Z0-9]+$/;
-  let requiredFields = [
+  const nameRegex = /^[a-zA-ZÀ-ÿ\s]+$/;
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+  const userRegex = /^[a-zA-Z][a-zA-Z0-9]*$/;
+  const hasMultipleWords = nameValue.trim().split(/\s+/).length > 1;
+  const requiredFields = [
     nameValue,
     cpfValue,
     emailValue,
@@ -135,35 +165,50 @@ async function SignUp(event) {
     passwordConfirmValue,
   ];
   let emptyFields = requiredFields.filter((field) => field == '');
-  console.log(emptyFields);
 
   if (emptyFields.length > 0) {
-    let popup = CreateElement('div', 'popup-error', 'Existem campos vazios!<br>Preencha todos os campos.');
+    let popup = CreateElement('div', 'popup-error', '❌ Existem campos vazios!<br>Preencha todos os campos.');
     RemovePopUpOfConsole(popup);
     return;
   }
-  if (!cpfRegex.test(cpfValue)) {
-    let popup = CreateElement('div', 'popup-error', 'CPF inválido!<br>O CPF deve estar no formato xxx.xxx.xxx-xx.');
+  else if (!nameRegex.test(nameValue) || !hasMultipleWords) {
+    let popup = CreateElement('div', 'popup-error', '❌ Nome inválido!<br>O nome deve começar com uma letra, ter no mínimo 2 palavras e conter apenas letras e espaços.');
+    RemovePopUpOfConsole(popup);
+    return;
+  }
+  else if (!cpfRegex.test(cpfValue)) {
+    let popup = CreateElement('div', 'popup-error', '❌ CPF inválido!<br>O CPF deve estar no formato xxx.xxx.xxx-xx.');
+    RemovePopUpOfConsole(popup);
+    return;
+  }
+  else if (!emailRegex.test(emailValue)) {
+    let popup = CreateElement('div', 'popup-error', '❌ E-mail inválido.<br>Por favor, insira um endereço de e-mail no formato correto (ex: nome@dominio.com).');
+    RemovePopUpOfConsole(popup);
+    return;
+  }
+  else if (!userRegex.test(userValue)) {
+    let popup = CreateElement('div', 'popup-error', '❌ Usuário inválido!<br>O nome de usuário deve começar com uma letra e conter apenas letras e números.');
+    RemovePopUpOfConsole(popup);
+    return;
+  }
+  else if (passwordValue != passwordConfirmValue) {
+    let popup = CreateElement('div', 'popup-error', '❌ As senhas não coincidem!');
     RemovePopUpOfConsole(popup);
     return;
   } 
   else if (!passwordRegex.test(passwordValue)) {
-    let popup = CreateElement('div', 'popup-error', 'Sua senha é fraca! Ela deve conter mais de 8 caracteres, ao menos um especial, uma letra e um número.');
+    let popup = CreateElement('div', 'popup-error', '❌ Sua senha é fraca! Ela deve conter mais de 8 caracteres, ao menos um especial, uma letra e um número.');
     RemovePopUpOfConsole(popup);
     return;
   } 
-  else if (passwordValue === passwordConfirmValue) {
-    let popup = CreateElement('div', 'popup-error', 'Cadastro realizado com sucesso!<br>Você será redirecionado para a página de login.');
+  else {
+    let popup = CreateElement('div', 'popup-sucess', '✅ Cadastro realizado com sucesso!<br>Você será redirecionado para a página de login.');
     RemovePopUpOfConsole(popup);
 
     // Outras validações
     // Salvar os dados em memória
     await Sleep(1800);
     window.location.href = "index.html";
-  } 
-  else {
-    let popup = CreateElement('div', 'popup-error', 'As senhas não coincidem!');
-    RemovePopUpOfConsole(popup);
   }
 }
 
